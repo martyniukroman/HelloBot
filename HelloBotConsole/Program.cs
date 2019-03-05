84be318bcff48175894ddf67e29cc2087ac85e46
Мартинюк Роман Олександрович <r.martyniuk@officekiev.fozzy.lan>
2019-03-05 14:47:50 +0200

    solution initialised

---
+++ HelloBotConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace HelloBotConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("760620360:AAGq9KIBMccVIGo0Rbggq3Hn88ehgLVNq38");
+            var me = botClient.GetMeAsync().Result;
+
+            Console.WriteLine($"Hello World I'm the:  ID:{me.Id} / FirstName:{me.FirstName} / Username:{me.Username}!");
+        }
+    }
+}
